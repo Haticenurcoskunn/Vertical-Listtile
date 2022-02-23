d499f797693d5c5791f6ce2ec39db580d65bf324
Hatice Nur <70744158+Haticenurcoskunn@users.noreply.github.com>
2022-02-23 19:38:06 +0300

    added example

--- example/example.dart
+++ example/example.dart
@@ -2,6 +2,33 @@
 
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: VerticalListtile(
+        backgroundcolor: Colors.grey.shade300,
+        borderRadius: 20,
+        containerHeight: 350,
+        containerWidth: 200,
+        startWidget: myStartWidget(),
+        centerWidget: myCenterWidget(),
+        finishWidget: myFinishWidget(),
+      ),
+    );
+  }
+}
+
 class VerticalListtile extends StatefulWidget {
   final Color backgroundcolor;
   final double borderRadius;
@@ -54,3 +81,21 @@ class _MyHomePageState extends State<VerticalListtile> {
     );
   }
 }
+
+Image myStartWidget() {
+  return Image.network(
+    "https://i.ytimg.com/vi/iwMqsKDTdQ0/maxresdefault.jpg",
+    width: 150,
+    height: 200,
+  );
+}
+
+Text myCenterWidget() => Text(
+      "HELLO GUYS I'M PLU ",
+      style: TextStyle(fontSize: 20, color: Colors.red.shade300),
+    );
+Icon myFinishWidget() => Icon(
+      Icons.add_reaction_sharp,
+      size: 30,
+      color: Colors.red.shade300,
+    );
